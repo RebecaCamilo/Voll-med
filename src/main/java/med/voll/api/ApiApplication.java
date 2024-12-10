f7355b950f7056ccec0dfdc85dbead99dd3a42d0
RebecaCamilo <rebecacamilo3@gmail.com>
2024-12-09 22:03:35 -0300

    feat: code to generate pass to save at database

--- src/main/java/med/voll/api/ApiApplication.java
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -1,7 +1,11 @@
 package med.voll.api;
 
+import med.voll.api.infra.security.SecurityConfigurations;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -9,5 +13,20 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
+
+//	@Bean
+//	public CommandLineRunner run(PasswordEncoder passwordEncoder) {
+//		return args -> {
+//			// Senha original
+//			String rawPassword = "teste";
+//
+//			// Criptografar a senha
+//			String encodedPassword = passwordEncoder.encode(rawPassword);
+//
+//			// Imprimir a senha original e criptografada no console
+//			System.out.println("Senha original: " + rawPassword);
+//			System.out.println("Senha criptografada: " + encodedPassword);
+//		};
+//	}
 
 }
